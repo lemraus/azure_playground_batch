db70bea132b006f95d2591e2db93b17e0e7fa601
lemraus <armel.chausse@student.ecp.fr>
2020-04-07 10:29:51 +0200

    Changing config_resources.py

--- config_resources.py
+++ config_resources.py
@@ -8,5 +8,5 @@ batch = {
 
 blob_container = {
     "url": "https://centralesupelecst7.blob.core.windows.net/results",
-    "sas_token": "?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-03-28T00:58:39Z&st=2020-03-14T16:58:39Z&spr=https&sig=SHhaiQ4XvDaPazP1XneTXSHu3OCIr%2Faul6H9M9LUrmM%3D"
+    "sas_token": "?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-07-05T16:53:21Z&st=2020-04-05T08:53:21Z&spr=https&sig=fXylwGQOv1zNDyCAo3xPdRS29sB0teGeMrSqff1Y8ps%3D"
 }
